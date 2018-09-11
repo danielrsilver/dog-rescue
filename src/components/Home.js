2d8e401841cefda7010a64c0ae2de82047f70a7f
Maria Alves <alvesdclaro@gmail.com>
2018-09-11 19:02:00 -0400

    updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,13 @@ export default class Home extends Component {
         <h2>There are thousands of adoptable pets looking for a lovable home. Help a dog find their new home today!</h2>
         <h2 className="homeparagraph1">Dogfinder is a way to centralize canine rescuing nationally. We saw a flaw in the adoption process for animals. Dogfinder gives you the ability to search all rescue groups nationally in one location. You can be as specific as the dogs breed, gender, age and more! All you need to do is simply fill out the form and you're one step closer to adopting your dream dog! </h2>
 
+
+            <div class="flex-container">
+              <h1>Why DogFinder?</h1>
+            </div>
+
           <div class="flex-container">
+
       <div>
         <section>
       <h1>Planing to Rescue a Dog?</h1>
@@ -26,16 +32,16 @@ export default class Home extends Component {
       <div>
         <article>
       <h1>Chechlist for the New Addition</h1>
-    <p>Help to make the transition as smooth as possible</p>
+    <p>Make the transition as smooth as possible</p>
      <i class="fas fa-check fa-2x"></i>
-       </article>
+     </article>
       </div>
 
       <div>
 
          <aside>
        <h1>Dog Adoption</h1>
-        <p>Get tips on figuring out who you should adopt</p>
+        <p>Tips on figuring out who you should adopt</p>
         <i class="fas fa-heart fa-2x"></i>
          </aside>
       </div>
