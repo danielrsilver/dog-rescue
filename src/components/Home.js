f0b593ec2ac6991a333a91f12c4fb7cb6f161970
Maria Alves <alvesdclaro@gmail.com>
2018-09-11 19:24:21 -0400

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,14 @@ export default class Home extends Component {
   render(){
     return(
       <div className="homepage">
-        <h2>
-          <a href="./search">Rescue a Dog Today!</a>
-        </h2>
-          <p className="homeparagraph1">Dogfinder is a way to centralize canine rescuing nationally. We saw a flaw in the adoption process for animals. Dogfinder gives you the ability to search all rescue groups nationally in one location. You can be as specific as the dogs breed, gender, age and more! All you need to do is simply fill out the form and you're one step closer to adopting your dream dog! </p>
+
+        <div id="bottom" class="parallax">
+          <h2>
+            <a href="./search">Rescue a Dog Today!</a>
+          </h2>
+            <p className="homeparagraph1">Dogfinder is a way to centralize canine rescuing nationally. We saw a flaw in the adoption process for animals. Dogfinder gives you the ability to search all rescue groups nationally in one location. You can be as specific as the dogs breed, gender, age and more! All you need to do is simply fill out the form and you're one step closer to adopting your dream dog! </p>
+
+        </div>
         <h1>DogFinder</h1><i class="fas fa-bone"></i>
         <h2>There are thousands of adoptable pets looking for a lovable home. Help a dog find their new home today!</h2>
         <h2 className="homeparagraph1">Dogfinder is a way to centralize canine rescuing nationally. We saw a flaw in the adoption process for animals. Dogfinder gives you the ability to search all rescue groups nationally in one location. You can be as specific as the dogs breed, gender, age and more! All you need to do is simply fill out the form and you're one step closer to adopting your dream dog! </h2>
@@ -21,7 +25,7 @@ export default class Home extends Component {
 
           <div class="flex-container">
 
-      <div>
+      <div >
         <section>
       <h1>Planing to Rescue a Dog?</h1>
      <p>Finding the right Dog</p>
@@ -29,7 +33,7 @@ export default class Home extends Component {
     </section>
       </div>
 
-      <div>
+      <div >
         <article>
       <h1>Chechlist for the New Addition</h1>
     <p>Make the transition as smooth as possible</p>
