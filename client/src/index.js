19e139525a32699ca0570cdd9a31f1fbf9e2c13a
Daniel Silver <danielreedsilver@gmail.com>
2018-09-12 13:02:37 -0400

    changed folder structure and added script for heroku deployment to package.json

---
+++ client/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import App from './App';
+import './css/index.css';
+import './css/DogForm.css';
+
+const Root = () => {
+  return(
+    <Router>
+      <App />
+    </Router>
+  )
+}
+
+render(<Root />, document.getElementById('root'));
